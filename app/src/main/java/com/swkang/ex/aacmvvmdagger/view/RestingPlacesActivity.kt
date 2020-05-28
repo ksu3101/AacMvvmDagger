9157b65569350af6e0f71cc0b9c4a673d3e0e197
beemo <burkdog@naver.com>
2020-05-28 18:17:09 +0900

    Initial commit

---
+++ app/src/main/java/com/swkang/ex/aacmvvmdagger/view/RestingPlacesActivity.kt
@@ -0,0 +1,18 @@
+package com.swkang.ex.aacmvvmdagger.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.swkang.ex.aacmvvmdagger.R
+
+/**
+ * @author kangsungwoo
+ * @since 5/27/2020
+ */
+class RestingPlacesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.restingplaces_activity)
+    }
+
+}
